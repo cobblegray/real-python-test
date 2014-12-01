31f209e24fa2993d45d7289a40aa28968f17f408
David Gray <dgray71@gmail.com>
2014-12-01 15:14:47 +0000

    flask-db001-p64

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
-
 def hello_world():
 	return "hello worldl.."
+
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query.upper()
+
+
 
 app.run()
 
